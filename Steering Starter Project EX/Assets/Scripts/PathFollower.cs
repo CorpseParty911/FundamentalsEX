8021621c825aba0afd3bf256f64873ec83ecf313
CorpseParty911 <49690591+CorpseParty911@users.noreply.github.com>
2021-02-04 14:01:56 -0700

    Add Unity Project

---
+++ Steering Starter Project EX/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : Kinematic
+{
+    PathFollow myMoveType;
+    LookWhereGoing myRotateType;
+
+    public GameObject[] path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMoveType = new PathFollow();
+        myMoveType.character = this;
+        myMoveType.path = path;
+        myMoveType.target = path[0];
+
+        myRotateType = new LookWhereGoing();
+        myRotateType.character = this;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+        steeringUpdate.angular = myRotateType.getSteering().angular;
+        base.Update();
+    }
+}
